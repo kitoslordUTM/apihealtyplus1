e9fc6e8f4357f87704cb49b6e8a16002067bc5bc
KairosL0rd32 <djjuliomex11@gmail.com>
2025-01-09 23:11:37 -0600

    fix(db.j777f)

--- dist/database/db.js
+++ dist/database/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const postgresProfile = new pg_1.Pool({
-    connectionString: 'postgresql://admin:qnahbhB9CWZrBlcuIpn0A0BvCgeeJy10@dpg-cuwadgdumphs7380gdag-a.oregon-postgr/examenbdd',
+    connectionString: 'postgresql://admin:qnahbhB9CWZrBlcuIpnOA0BvCgeeJy1Q@dpg-cu0adgdumphs7380gdag-a.oregon-postgres.render.com/examenbdd',
     ssl: {
         rejectUnauthorized: false
     }
@@ -12,6 +12,6 @@ postgresProfile.connect()
     console.log('Conexión a la base de datos establecida con éxito 777');
 })
     .catch(err => {
-    console.error('Error al conectar a la base de datos:', err.stack);
+    console.error('Error al conectar a la base de datos 88:', err.stack);
 });
 exports.default = postgresProfile;
--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 
 const postgresProfile = new Pool({
-    connectionString: 'postgresql://admin:qnahbhB9CWZrBlcuIpn0A0BvCgeeJy10@dpg-cuwadgdumphs7380gdag-a.oregon-postgr/examenbdd',
+    connectionString: 'postgresql://admin:qnahbhB9CWZrBlcuIpnOA0BvCgeeJy1Q@dpg-cu0adgdumphs7380gdag-a.oregon-postgres.render.com/examenbdd',
     ssl: {
         rejectUnauthorized: false
     }
@@ -13,7 +13,7 @@ postgresProfile.connect()
     console.log('Conexión a la base de datos establecida con éxito 777');
   })
   .catch(err => {
-    console.error('Error al conectar a la base de datos:', err.stack);
+    console.error('Error al conectar a la base de datos 88:', err.stack);
   });
 
 export default postgresProfile;

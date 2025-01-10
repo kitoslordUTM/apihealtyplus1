01173a86197fb18c66fcb4d38fa2d386078de503
KairosL0rd32 <djjuliomex11@gmail.com>
2025-01-09 23:09:07 -0600

    fix(db.jsrf)

--- dist/database/db.js
+++ dist/database/db.js
@@ -1,23 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-// Configura la conexión a tu base de datos en Render.com
 const postgresProfile = new pg_1.Pool({
-    user: 'admin', // Reemplaza con el username de Render.com
-    host: 'dpg-cuwadgdumphs7380gdag-a', // Reemplaza con el hostname de Render.com
-    database: 'examenbd', // Reemplaza con el nombre de la base de datos de Render.com
-    password: 'qnahbhB9CWZrBlcuIpn0A8BvCgeeJy10', // Reemplaza con la contraseña de Render.com
-    port: 5432, // Puedes mantener el puerto 5432, a menos que Render te indique otro
+    connectionString: 'postgresql://admin:qnahbhB9CWZrBlcuIpn0A0BvCgeeJy10@dpg-cuwadgdumphs7380gdag-a.oregon-postgr/examenbdd',
     ssl: {
-        rejectUnauthorized: false // Necesario para algunas configuraciones de Render
+        rejectUnauthorized: false
     }
 });
-// Probar la conexión
-postgresProfile.connect((err, client, release) => {
-    if (err) {
-        return console.error('Error al conectar a la base de datos:', err.stack);
-    }
-    console.log('Conexión exitosa a la base de datos en Render.com');
-    release();
+postgresProfile.connect()
+    .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito 777');
+})
+    .catch(err => {
+    console.error('Error al conectar a la base de datos:', err.stack);
 });
 exports.default = postgresProfile;
